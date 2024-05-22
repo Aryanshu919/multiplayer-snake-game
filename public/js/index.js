25e919ab3e1d30bea31d6143526582e861ea377a
Aryan <92309448+Aryanshu919@users.noreply.github.com>
2024-05-22 19:49:42 +0530

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,5 @@
 // Making Connection
-const socket = io.connect("http://localhost:3000");
+const socket = io.connect("https://multiplayer-snake-game-n1ne.vercel.app/");
 socket.emit("joined");
 
 let players = []; // All players in the game
