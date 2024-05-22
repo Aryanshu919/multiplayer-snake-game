fb15542be554f89e3cbb9b1df7a82a74cf58cce5
Aryan Kumar <aryanshu919@gmail.com>
2024-05-22 19:38:24 +0530

    added cors package

--- server.js
+++ server.js
@@ -2,12 +2,22 @@ const express = require('express');
 const { createServer } = require('node:http');
 const socket = require('socket.io');
 const { Server } = require('socket.io')
+const cors = require("cors");
 
+require('dotenv').config();
 const app = express();
-const PORT = 3000 || process.env.PORT;
+
+const PORT = process.env.PORT;
 const server = createServer(app)
 
 app.use(express.static('public'));
+app.use(
+    cors({
+      origin: "*",
+      methods: 'GET,POST,PUT,DELETE',
+      credentials: true,
+    }),
+  );
 
 const io = new Server(server);
 
